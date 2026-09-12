b61b24c9e83b6c6320d115322cc986f64c64ea95
Obskyne <justinaaron612@gmail.com>
2026-09-12 13:48:14 -0600

    Initial Commit - Upload This Shit

---
+++ Scripts/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+var SPEED: float = 85.0
+var JUMP_VELOCITY: float = -350.0
+var GRAVITY: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	# Handles jump longevity
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y /= 2
+
+
+	# Get the input direction and handle the movement/deceleration.
+	
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	var current_speed = SPEED * 1.5 if Input.is_action_pressed("sprint") else SPEED
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		elif direction != 0:
+			animated_sprite.play("walk")
+	else:
+		animated_sprite.play("idle")
+	
+	
+	if direction:
+		velocity.x = direction * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+	
+	move_and_slide()
---
+++ Scripts/void.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(_body: Node2D) -> void:
+	timer.start()
+	print("You Died Nigga!")
+
+
+func _on_timer_timeout() -> void:
+	print("Bitch")
+	get_tree().reload_current_scene()
